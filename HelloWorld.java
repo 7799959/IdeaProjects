c6037a455cd2de66a4ac9601ee80af878b4a43e5
7799959 <2736850437@qq.com>
2021-12-05 15:42:22 +0800

    my expendense

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld!!");
+            System.out.println("I");
+            System.out.println("LOVE");
+            System.out.println("YOU");
+    }
+}
